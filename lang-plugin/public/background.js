a2014f543e38f1b24d628054ab03e9265000f0fe
Yana <yana.lushch@securekey.com>
2024-02-19 15:16:35 +0100

    init front end project plugin

---
+++ lang-plugin/public/background.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+
+// This code is responsible for the automated execution of certain actions
+// on the chrome tabs upon their update, based on settings saved in the
+// local storage. The settings include whether an action is active, whether
+// it's a "stepper" (which influences the number of action calls),
+// and a click count that determines the amount of action invocations if
+// the action is a stepper.
+
+  
